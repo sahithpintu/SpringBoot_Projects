49b9b3264884b56d8247700fa5f4eeeda95c1b0b
vjella <vjella@DIN65000560.corp.capgemini.com>
2020-07-24 12:06:10 +0530

    Spring Boot Cloud Data Flow using Kafka

---
+++ SpringBootCloudDataFlowKafkaProducerService/src/test/java/com/javatechie/springboot/clouddata/flow/kafka/producerservice/SpringBootCloudDataFlowKafkaProducerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.springboot.clouddata.flow.kafka.producerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCloudDataFlowKafkaProducerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
