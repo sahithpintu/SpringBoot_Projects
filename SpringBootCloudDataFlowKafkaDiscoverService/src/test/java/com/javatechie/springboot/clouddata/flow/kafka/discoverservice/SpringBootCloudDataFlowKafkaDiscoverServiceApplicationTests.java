ed56d0c8284cdc2976341d45f00bea6a75bb0133
vjella <vjella@DIN65000560.corp.capgemini.com>
2020-07-24 12:05:42 +0530

    Spring Boot Cloud Data Flow using Kafka

---
+++ SpringBootCloudDataFlowKafkaDiscoverService/src/test/java/com/javatechie/springboot/clouddata/flow/kafka/discoverservice/SpringBootCloudDataFlowKafkaDiscoverServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechie.springboot.clouddata.flow.kafka.discoverservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCloudDataFlowKafkaDiscoverServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
